7575493bc6fd8599a6a21552599189f888e5b715
Nate Lunt <nate.lunt04@gmail.com>
2020-11-08 20:32:00 -0700

    bothParts-complete

---
+++ Part2/index.js
@@ -0,0 +1,11 @@
+console.log("The mouse always wins.");
+
+let idInput = document.getElementById('idInput')
+
+let colorInput = document.getElementById('colorInput')
+
+function setCard(){
+    let card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value;
+}
+
